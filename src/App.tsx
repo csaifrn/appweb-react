8aae884778a13c6bbf4697a21765edaf9ba657db
Leonardo Minora <leonardo.minora@gmail.com>
2022-09-08 11:10:12 -0300

    using vscode to commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Label = function (props: any) {
+   return (
+      <h2>
+         {props.title} : {props.value}
+      </h2>
+   );
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+class LabelArrow extends React.Component {
+   render() {
+      return <h2>Arrow teste {this.props.count}</h2>;
+   }
 }
 
-export default App
+const App = (props: any) => {
+   const [count, setCount] = useState(0);
+
+   return (
+      <div className="App">
+         <div>
+            <a href="https://vitejs.dev" target="_blank">
+               <img src="/vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+               <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+         </div>
+         <h1>Vite + React</h1>
+         <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+               count is {count}
+            </button>
+            <Label value={count} title="meu texto" />
+            <LabelArrow count={count} />
+         </div>
+      </div>
+   );
+};
+
+export default App;
